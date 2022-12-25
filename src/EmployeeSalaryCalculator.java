a38fec2fc56910c01ad88320b86320ca094d5011
David <davidukpetteh@gmail.com>
2022-12-25 03:56:39 +0100

    added

--- src/EmployeeSalaryCalculator.java
+++ src/EmployeeSalaryCalculator.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class EmployeeSalaryCalculator {
+    private static int numbersOfHoursWorked;
+    private static int hourlyRates;
+
+
+    public static void main(String[] args) {
+        numbersOfHoursWorked = 0;
+        hourlyRates = 0;
+
+
+        Scanner input = new Scanner(System.in);
+        for (int employee = 1; employee <= 3; employee++,System.out.println()) {
+            System.out.println("enter hours worked for employee" + employee);
+            numbersOfHoursWorked = input.nextInt();
+            System.out.println("enter hourly rate for employee " + employee);
+            hourlyRates = input.nextInt();
+            grossPay();
+
+        }
+
+
+        }
+
+    private static void grossPay() {
+double grossPay;
+        if (numbersOfHoursWorked <= 40) {
+             grossPay = numbersOfHoursWorked * hourlyRates;
+        } else {
+             grossPay = (40 * hourlyRates) + ((numbersOfHoursWorked - 40) * hourlyRates * 1.5);
+        }
+        System.out.println("\nthe grossPay is " + grossPay);
+
+    }
 
 }
+
--- test/EmployeeSalaryCalculatorTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class EmployeeSalaryCalculatorTest {
-    @Test
-    void createNewCalculator(){
-        EmployeeSalaryCalculator firstEmployee = new EmployeeSalaryCalculator();
-
-    }
-}
