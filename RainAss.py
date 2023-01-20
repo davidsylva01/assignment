80fee5fa523d4ba5517341d691cd4822f6931476
David Sylva <davidukpetteh@gmail.com>
2023-01-20 06:43:49 +0100

    initial commit

---
+++ RainAss.py
@@ -0,0 +1,8 @@
+rain_entry = input ("enter volume of rainfall in inches: ")
+inch = int(rain_entry)
+gallon = inch / 231
+
+
+
+print ("the volume of water accumulated on the land is:", gallon, "gallons")
+
